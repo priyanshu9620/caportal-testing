e2d011ecc034c0443340aa6df3acc8b3aa4e3b7b
DecryptAG <akshat.goel.civ21@itbhu.ac.in>
2023-10-17 19:49:38 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='underline font-extrabold'>KASHIYATRA CAMPUS AMBASSADOR</div>
+    </>
+  )
+}
+
+export default App
